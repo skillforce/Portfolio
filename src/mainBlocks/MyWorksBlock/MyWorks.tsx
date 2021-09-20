b80849b5b6ba7584ba5a4cd0dd4272cb960378ee
skillforce@mail.ru <skillforce@mail.ru>
2021-09-20 16:34:47 +0300

    with cases+adaptation

--- src/mainBlocks/MyWorksBlock/MyWorks.tsx
+++ src/mainBlocks/MyWorksBlock/MyWorks.tsx
@@ -1,5 +1,7 @@
 import s from './MyWorks.module.css';
 import {WorksBox} from './WorksBox/WorksBox';
+import socialNet from './imgWorks/socialNetWork.png';
+import counter from './imgWorks/Counter.png';
 
 
 const {
@@ -12,15 +14,23 @@ const {
 
 
 export const MyWorks = () => {
+
+
+    const socialPrjImg={
+        backgroundImage:`url(${socialNet})`
+    }
+    const counterPrjImg={
+        backgroundImage:`url(${counter})`
+    }
     return (<div className={blockTitle}>
             <div className={container}>
                 <div className={MyWorksBlock}>
                     <div className={MyWorksTitle}>
-                        <p>My works</p>
+                        <span>My</span> <p>works</p>
                     </div>
                     <div className={MyWorksMain}>
-                    <WorksBox/>
-                    <WorksBox/>
+                    <WorksBox styleImg={socialPrjImg} descriptions={'Look at my social network'} name={'Samyrai way project'}/>
+                    <WorksBox styleImg={counterPrjImg} descriptions={'Look at my counter'} name={'Counter(with Redux using)'}/>
                     </div>
                 </div>
             </div>
--- src/mainBlocks/MyWorksBlock/WorksBox/WorksBox.tsx
+++ src/mainBlocks/MyWorksBlock/WorksBox/WorksBox.tsx
@@ -10,20 +10,28 @@ const {
     MyWorksItemMainName,
 } = s;
 
+type WorksBoxType={
+    name:string
+    descriptions:string
+    styleImg:any
+}
 
-export const WorksBox = () => {
+
+export const WorksBox = (props:WorksBoxType) => {
+    const {name,descriptions,styleImg}=props;
+
     return (<div className={MyWorksItem}>
             <div className={MyWorksItemTitle}>
-                <div className={MyWorksItemTitleImg}>
+                <div style = {styleImg} className={MyWorksItemTitleImg}>
                     <Btns title={'Look'}/>
                 </div>
             </div>
             <div className={MyWorksItemMain}>
                 <div className={MyWorksItemMainName}>
-                    <p>Project name</p>
+                    <p>{name}</p>
                 </div>
                 <div className={MyWorksItemMainDescr}>
-                    <p>short descriptions</p>
+                    <p>{descriptions}</p>
                 </div>
             </div>
         </div>
