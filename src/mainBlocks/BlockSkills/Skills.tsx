d18a0b4b6ec0ce5b038cd562ec7f174d431be56d
skillforce@mail.ru <skillforce@mail.ru>
2021-09-20 14:58:56 +0300

    +mySkill

--- src/mainBlocks/BlockSkills/Skills.tsx
+++ src/mainBlocks/BlockSkills/Skills.tsx
@@ -1,5 +1,11 @@
 import s from './Skills.module.css';
 import {SkillBox} from './skillBox/SkillBox';
+import reactImg from './skillBox/skillImgIcon/REACT.png';
+import JSImg from './skillBox/skillImgIcon/JS.png';
+import HTMLImg from './skillBox/skillImgIcon/HTML.png';
+import CSSImg from './skillBox/skillImgIcon/CSS.png';
+import reduxImg from './skillBox/skillImgIcon/REDUX.png';
+import toolsImg from './skillBox/skillImgIcon/TOOLS.png';
 
 
 const {skillsTitle, blockTitle, skillsMain, container,skillsBlock} = s;
@@ -13,9 +19,11 @@ export const Skills = () => {
                     <span>My</span> <p>skills</p>
                 </div>
                 <div className={skillsMain}>
-                    <SkillBox skill={'React'} skillDescr={'detailed skill descriptions'} skillImg={'none'}/>
-                    <SkillBox skill={'JS'} skillDescr={'detailed skill descriptions'} skillImg={'none'}/>
-                    <SkillBox skill={'HTML'} skillDescr={'detailed skill descriptions'} skillImg={'none'}/>
+                    <SkillBox skill={'Native JS'} skillDescr={''} skillImg={JSImg} progress={60}/>
+                    <SkillBox skill={'HTML 5'} skillDescr={''} skillImg={HTMLImg} progress={70}/>
+                    <SkillBox skill={'CSS 3'} skillDescr={''} skillImg={CSSImg} progress={70}/>
+                    <SkillBox skill={'React'} skillDescr={''} skillImg={reactImg} progress={65}/>
+                    <SkillBox skill={'Redux'} skillDescr={''} skillImg={reduxImg} progress={60}/>
                 </div>
                 </div>
             </div>
--- src/mainBlocks/BlockSkills/skillBox/SkillBox.tsx
+++ src/mainBlocks/BlockSkills/skillBox/SkillBox.tsx
@@ -1,28 +1,51 @@
 import s from './SkillBox.module.css';
+import {buildStyles, CircularProgressbar} from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 
-const {skillItem, skillItemTitle, skillItemMain,skillItemTitleImg} = s;
+const {
+    skillItem,
+    skillItemTitle,
+    skillItemTitleImg,
+    circleBar
 
-type skillBoxPropsType ={
-    skill:string
+} = s;
+
+type skillBoxPropsType = {
+    skill: string
     skillDescr: string
-    skillImg:string
+    skillImg: string
+    progress: number
+}
+
+const ProgressBarStyle={
+    root:'white',
+    path: 'red',
+    trail: "gray",
+    text : 'yellow',
+    background: 'green'
 }
 
 
-export const SkillBox = (pr:skillBoxPropsType) => {
 
-    const{skill,skillDescr,skillImg}=pr;
+export const SkillBox = (pr: skillBoxPropsType) => {
+
+    const {skill, skillDescr, skillImg, progress} = pr;
     return (<div className={skillItem}>
             <div className={skillItemTitle}>
                 <div className={skillItemTitleImg}>
-                    <img src={skillImg} alt=""/>
+                    <img src={skillImg} alt={skillDescr}/>
                 </div>
                 <p>{skill}</p>
-            </div>
-            <div className={skillItemMain}>
-                <p>{skillDescr}</p>
+                <CircularProgressbar styles={buildStyles({
+                    textSize: '25px',
+                    pathTransitionDuration: 0.5,
+                    pathColor: `#ffb400`,
+                    textColor: '#fff',
+                    trailColor: '#113',
+                })} className={circleBar} value={progress} text={`${progress}%`} strokeWidth={10}/>
             </div>
         </div>
+
     )
 }
